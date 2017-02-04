e500755eca045b33a1ffe491f90cca62e9486732
wwr@pc <weiren.wan@live.com>
2017-02-04 12:29:46 +0800

    ...

--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -8,28 +8,29 @@ namespace Test
     {
         public static void Main(string[] args)
         {
-            var template = @"Hello @Model.Name Welcome to  repository
-                        @foreach(string s in Model.Lst)
-                        {
-                            <a href=""@s"">@s</a>
-                        }";
+            var template = @"<h1>Hello @Model.Name Welcome to  repository</h1>
+                            <ul>
+                            @foreach(var s in Model.Color)
+                            {
+                                <li>
+                                    <a href=""@s"">@s</a>
+                                </li>
+                            }
+                            </ul>";
 
             var model = new
             {
-                Name = "John Doe",
-                Title = "RazorLight",
-                Age = 23,
-                Lst = new string[] { "aaa", "bbb", "ccc" }
+                Name    =   "John Doe",
+                Age     =   23,
+                Color   =   new string[] { "red", "yellow", "blue" }
             };
+
+            //var str = Engine.Parse(template, model);
 
             var json = Newtonsoft.Json.JsonConvert.SerializeObject(model);
 
-            //var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(json);
-
             var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<ExpandoObject>(json);
-
             
-
             var str = Engine.Parse(template, jsonObj);
 
             Console.Write(str);
